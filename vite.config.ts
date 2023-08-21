db2aca1b603b701bf2490fba1907f0740c71b640
biswajitmee <biswajit.mee@gmail.com>
2023-08-21 23:01:01 +0530

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/errorburner",
   resolve: {
     alias: {
       // Add aliases for your project directories here
